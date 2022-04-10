f835bf3541feab7e48a59d0f8e6d9452cb5d9d72
cropacoulus <desmar217@gmail.com>
2022-04-10 16:23:24 +0700

    CRUD USER

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,7 @@ Route::prefix('admin')
     ->group(function() {
         Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('admin-dashboard');
         Route::resource('category', App\Http\Controllers\Admin\CategoryController::class);
+        Route::resource('user', App\Http\Controllers\Admin\UserController::class);
     });
 
 Auth::routes();
