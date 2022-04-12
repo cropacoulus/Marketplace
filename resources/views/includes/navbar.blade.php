4764233b70d1b5345ca6c6e2a79f20311c2ab393
cropacoulus <desmar217@gmail.com>
2022-04-13 02:26:53 +0700

    conditional navbar after login

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -17,13 +17,67 @@
                 <li class="nav-item">
                     <a href="#" class="nav-link">Rewards</a>
                 </li>
-                <li class="nav-item">
-                    <a href="register.html" class="nav-link">Sign up</a>
-                </li>
-                <li class="nav-item">
-                    <a href="login.html" class="btn btn-success nav-link px-4 text-white">Sign in</a>
-                </li>
+                @guest
+                    <li class="nav-item">
+                        <a href="{{ route('register') }}" class="nav-link">Sign up</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('login') }}" class="btn btn-success nav-link px-4 text-white">Sign in</a>
+                    </li>
+                @endguest
             </ul>
+
+            @auth
+                <!-- Dekstop Menu -->
+                <ul class="navbar-nav d-none d-lg-flex">
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown">
+                            <img src="images/icon-user.png" alt="User" class="rounded-circle mr-2 profile-picture">
+                            Hi, {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu">
+                            <a href="{{ route('dashboard') }}" class="dropdown-item">Dashboard</a>
+                            <a href="{{ route('dashboard-settings-account') }}" class="dropdown-item">Settings</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a href="cart.html" class="nav-link d-inline-block mt-2">
+                            <img src="{{ asset('images/icon-cart-empty.svg') }}" alt="Cart">
+                        </a>
+                    </li>
+                </ul>
+                <!-- End Dekstop Menu -->
+        
+                <!--- Mobile Menu -->
+                <ul class="navbar-nav d-block d-lg-none">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            Hi, Angga
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            Cart
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('logout') }}" class="nav-link d-inline-block">
+                            Logout
+                        </a>
+                    </li>
+                </ul>
+                <!-- End Mobile Menu -->    
+            @endauth
         </div>
     </div>
 </nav>
